5fa9605192b3d9e78a0ac02e08a074d466ec4acc
alvinsarkadi <35042327+alvinsarkadi@users.noreply.github.com>
2018-01-30 14:02:17 -0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,12 @@
+while True;
+    print("This program calculates the area of a rectangle")
+    length=float(input("Enter the length: "))
+    width=float(input("Enter the width"))
+    areaof_rectangle=length*width
+    print("The area of the rectangle is:")
+    print(areaof_rectangle)
+    user_input=input("would you like to perform a new calculation? Y/N?")
+    if user_input=="Y" or user_input=="Y":
+                continue
+    else:
+                break
